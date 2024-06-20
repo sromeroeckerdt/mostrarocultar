e1392343aa42a21914b36a8b12e6cd5697f5d3ec
Sofia Romero Eckerdt <164594784+sromeroeckerdt@users.noreply.github.com>
2024-06-20 02:31:01 -0300

    ocultarmostrarver1

---
+++ funct.js/showhide.js
@@ -0,0 +1,17 @@
+function Show(){
+    var imagen = document.getElementsByClassName("imagen") [0];
+    
+    if(imagen.style.visibility == "hidden"){
+        imagen.style.visibility = "visible";
+    } else{
+        imagen.style.visibility = "visible";
+    }
+}
+function Hide(){
+    var imagen = document.getElementsByClassName("imagen") [0];
+    if(imagen.style.visibility == "visible"){
+        imagen.style.visibility = "hidden";
+    } else{
+        imagen.style.visibility = "hidden";
+    }
+}
